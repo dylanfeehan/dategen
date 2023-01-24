a9343183b3af0049025c8f09d05b92636cc3bf1d
Dylan Feehan <dylanfeehan981@gmail.com>
2023-01-24 00:13:39 -0500

    Updates

---
+++ pages/blogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blogs = () => {
+  return (
+    <div>
+      <h1>There are no blogs. i just didn't feel like leaving out this part of the navbar tutorial i used</h1>
+      <a href="https://www.youtube.com/watch?v=rdIqZ8ocyhw" alt="cat go ahh">click me</a>
+    </div>
+  );
+};
+
+export default Blogs;
