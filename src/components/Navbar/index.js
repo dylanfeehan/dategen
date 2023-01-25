05ec3ace9150e028af18938fa90525522e22c61e
dylanfeehan <dylanfeehan981@gmail.com>
2023-01-24 23:28:05 -0500

    sickk

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -15,12 +15,13 @@ const MyNavbar = () => {
             <Navbar.Brand>
               <HomeLogo />
             </Navbar.Brand>
-            <Nav.Link as={Link} to="/about" className="nav-link">About</Nav.Link>
+            <Nav.Link as={Link} to="/about" className="">About</Nav.Link>
             <Nav.Link as={Link} to="/contact" className="nav-link">Contact</Nav.Link>
             <Nav.Link as={Link} to="/blogs" className="nav-link">Blog</Nav.Link>
 
             <NavDropdown title="Other" id="basic-nav-dropdown">
               <NavDropdown.Item as={Link} to="/sign-up">Sign Up</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/dylanfeehan/dategen" target="_blank">Source</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           </Container>
