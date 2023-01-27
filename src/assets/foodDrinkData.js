641c44dc9c84527cb20dbf36da03e50e52fe29f5
Dylan Feehan <dylanfeehan981@gmail.com>
2023-01-26 21:37:03 -0500

    finally have some good content

--- src/assets/foodDrinkData.js
+++ src/assets/foodDrinkData.js
@@ -4,31 +4,31 @@ export const foodDrinkData = [
   {
     title: "girasole",
     text: "enjoy a fantastic dinner in shadyside's premier italian restaurant",
-    image: require('./images/test_photo.jpg')
+    image: require('./images/girasole.jpg')
   },
   {
     title: "acacia",
     text: "enjoy a fantastic dinner in shadyside's premier italian restaurant",
-    image: require('./images/test_photo.jpg')
+    image: require('./images/acacia.jpg')
   },
   {
     title: "treat",
     text: "enjoy a fantastic dinner in shadyside's premier italian restaurant",
-    image: require('./images/test_photo.jpg')
+    image: require('./images/treat.jpg')
   },
   {
     title: "gi jin",
     text: "dine on the finest sushi in all of pittsburgh",
-    image: require('./images/test_photo.jpg')
+    image: require('./images/gijin.jpg')
   },
   {
-    title: "altius",
+    title: "allindia",
     text: "too much money in your wallet? enjoy a view of the city while blowing all of your money on average food",
-    image: require('./images/test_photo.jpg')
+    image: require('./images/allindia.jpg')
   },
   {
     title: "noodlehead",
     text: "enjoy a fantastic dinner in shadyside's premier italian restaurant",
-    image: require('./images/test_photo.jpg')
+    image: require('./images/noodlehead.jpg')
   }
 ]
