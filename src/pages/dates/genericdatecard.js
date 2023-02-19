ad2ef091b9256ab30de39070210292c73e6391eb
Dylan Feehan <dylanfeehan981@gmail.com>
2023-02-18 21:13:06 -0500

    woah finally

--- src/pages/dates/genericdatecard.js
+++ src/pages/dates/genericdatecard.js
@@ -1,4 +1,5 @@
 
+import axios from 'axios';
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
@@ -11,9 +12,24 @@ import {Link} from 'react-router-dom';
 class GenericDateCard extends React.Component {
   constructor(props)   {
     super(props)
-    this.state = {data: []};
+    this.state = {
+      dates: []
+    };
   }
 
+  // THIS IS A MESS. JUST FIGURE THE FRICK OUT how to uhh ,,, get the frickin data
+  componentDidMount() {
+    fetch("http://127.0.0.1:5000/getdates/", {
+      'methods': 'GET',
+      headers: {
+        'Content-Type': 'application/JSON'
+      }
+    })
+    .then(resp=>resp.json())
+    .then(data =>
+      this.setState({dates: data}));
+  };
+  /*
   componentDidMount() {
     fetch("http://127.0.0.1:5000/getdates/", {
       'methods': 'GET', 
@@ -26,11 +42,14 @@ class GenericDateCard extends React.Component {
     .then(resp=>this.setState(resp))
     .then(error=>console.log(error));
   };
-
+  */
 
 
   render() {
     const {data} = this.props;
+    let array = this.state
+    console.log("this is the idiot")
+    console.log(array.dates.length);
     return (
       <div>
         {data.map((card, index) => (
