bbdaaadcc42a29a93cf040d93eed9e4ad54cd5da
Dylan Feehan <dylanfeehan981@gmail.com>
2023-02-21 16:39:36 -0500

    just adding a todo for ... when a user uploads a date, they should also be given the option to jump to that date :)

--- src/pages/uploadSuccess.js
+++ src/pages/uploadSuccess.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 
+// TODO: if this is an upload, and the user just uplaoded a date, it would be nice if they could go to / see the date they just uplaoded :)
 const UploadSuccess = () => {
   const location = useLocation();
   const action = location.state.action;
