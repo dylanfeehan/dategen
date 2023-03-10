2000b774734497f9c15ae6bb45bf9e5fa1498192
Dylan Feehan <dylanfeehan981@gmail.com>
2023-03-10 13:30:52 -0500

    more cleanup of deployment junk, cleaned up the api dockerfile, got rid of the boot.sh file

--- boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ls -a
-
-# remove this comment if things don't workout without the venv
-#. ./venv/bin/activate
-
-#flask initdb # for fresh start
-# exec gunicorn -b local:5000 file:module
-exec gunicorn -b 0.0.0.0:8080 storage:app
