9e052ccace61d40357669f53e5fbeba38feabd44
Dylan Feehan <dylanfeehan981@gmail.com>
2023-03-16 02:08:08 -0400

    cleaned up a lot and added some comments

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import APIService from '../api/APIService';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
 import { Button } from 'react-bootstrap'
-import { useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import { firebaseConfig } from '../assets/firebaseConfig';
 import { useState } from 'react'
-import { getIdTokenResult } from 'firebase/auth';
 
 const Homepage = () => {
+    // reinitialize the firebase app for this page
     const app = firebase.initializeApp(firebaseConfig);
     const auth = getAuth(app)
+
+    // needed for getting the authenticated user since getUser is async
     const [user, setUser] = useState(null);
 
-    function verify(user) {
-        console.log('the button was clicked by ' + user.displayName);
-        const token = user.getIdToken(false)
-            .then((token) => {
-                console.log('here is token: ' + token);
-                APIService.Verify(token);
-            });
-    }
     function submitPost(user) {
         const datespecs = {
             title: "another one",
@@ -33,21 +25,22 @@ const Homepage = () => {
             directions: "verified directions",
         }
         user.getIdToken(false)
-        .then((token) => {
-            APIService.SubmitDate(datespecs, token)
-        })
-        .catch((error) => console.log(error))
-        
+            .then((token) => {
+                APIService.SubmitDate(datespecs, token)
+            })
+            .catch((error) => console.log(error))
+
     }
     function getPosts(user) {
         user.getIdToken(false)
-        .then((token) => {
-            const promise = APIService.GetDatesProtected(token);
-            promise.then((data) => console.log(data))
-        })
+            .then((token) => {
+                const promise = APIService.GetDatesProtected(token);
+                promise.then((data) => console.log(data))
+            })
         console.log('getting posts')
     }
 
+    // general flow for getting a user (setting component state)
     auth.onAuthStateChanged((user) => {
         if (user) {
             console.log('found user')
@@ -65,20 +58,13 @@ const Homepage = () => {
                 <div>
                     <h1>welcome {user.displayName}</h1>
                     <Button onClick={() => {
-                        verify(user);
-                    }}>Verify Myself</Button>
-                    <Button onClick={() => {
                         submitPost(user);
                     }}>Submit post</Button>
                     <Button onClick={() => {
                         getPosts(user);
                     }}>Get Dates</Button>
                 </div>
-            )
-                :
-                (
-                    <h1>fetching your creds...</h1>
-                )
+            ): (<h1>fetching your creds...</h1>)
             }
         </div>
     )
