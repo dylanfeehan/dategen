768bbf59974f12899439e81033299a33d066dcdc
Dylan Feehan <dylanfeehan981@gmail.com>
2023-03-16 15:10:55 -0400

    lots of renaming, next step is to rebuild the entire api around tokens

--- backend/storage.py
+++ backend/storage.py
@@ -1,3 +1,10 @@
+"""
+Standards: 
+    - 'jwt' refers to an serialized web token
+    - 'token' refers to a verified, deserialized web token
+        - other than middleware, 'token' always corresponds to a user in the applciation database
+
+"""
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,66 +14,87 @@ import firebase_admin
 from firebase_admin import credentials, auth
 
 from configmodule import DevelopmentConfig
-from models import db, ma, User, Dates, datesSchema, dateSchema, userSchema, usersSchema
+from models import db, ma, User, Post, postSchema, postsSchema, userSchema, usersSchema
 
 app = Flask(__name__)
-
 app.config.from_object(DevelopmentConfig())
-
 CORS(app)
-
 ma.init_app(app)
 db.init_app(app)
 
 path = '/home/dylan/projects/dategen/backend/dategen-admin.json'
 cred = credentials.Certificate(path)
-
 firebase_app = firebase_admin.initialize_app(cred)
 
+"""
+MIDDLEWARE
+Parameters
+----------
+token: a deserialized JWT coming from the verify_token middleware
 
+Action
+------
+creates a user, since one does not exist for the provided JWT
+
+"""
 def create_user(token):
     user = User(token['user_id'], token['name'], token['email'])
     db.session.add(user)
     db.session.commit()
 
+
+"""
+MIDDLEWARE
+Parameters
+----------
+Action
+------
+- retrieve token from request object
+- verify token 
+- calls create_user if none exists for token
+- sets request.token to the deserialized token
+"""
 @app.before_request
-def verify_request():
-    token = request.headers.get('Authorization')
-    print('token: ' + str(token))
-    if (token != None):
-        try: 
-            token = auth.verify_id_token(token)
-        except: 
+def verify_token():
+    jwt = request.headers.get('Authorization')
+    token = None
+    print('token: ' + str(jwt))
+    if (jwt != None):
+        print('well fuck')
+        try:
+            token = auth.verify_id_token(jwt)
+        except:
             return '', 500
 
         user = User.query.get(token['user_id'])
-        if(user == None):
+        if (user == None):
             create_user(token)
         request.token = token
 
 
-@app.route('/get_dates/', methods=['GET'])
-def get_dates_protected():
+@app.route('/get_posts/', methods=['GET'])
+def get_posts_protected():
     token = request.token
     user = User.query.get(token['user_id'])
-    dates = user.posts
-    print(dates)
-    return datesSchema.jsonify(dates)
+    posts = user.posts
+    print(posts)
+    return postsSchema.jsonify(posts)
 
 
-@app.route('/upload_date/', methods=['PUT'])
-def upload_date_protected():
+@app.route('/upload_post/', methods=['PUT'])
+def upload_post_protected():
     token = request.token
-    date_obj = request.get_json() 
-    date = Dates(token['user_id'], date_obj['title'], date_obj['type'],
-                 date_obj['site'], date_obj['details'],
-                 date_obj['reservations'], date_obj['notes'], date_obj['directions'])
+    post_info = request.get_json()
+    post = Post(token['user_id'], post_info['title'], post_info['type'],
+                 post_info['site'], post_info['details'],
+                 post_info['preparation'], post_info['notes'], post_info['location'])
 
     user = User.query.get(token['user_id'])
-    user.posts.append(date)
+    user.posts.append(post)
     db.session.add(user)
     db.session.commit()
     return '', 200
+
 
 @app.route('/verify/', methods=['PUT', 'GET'])
 def verify_token():
@@ -82,75 +110,63 @@ def verify_token():
     return jsonify({'error': 'i printed it...'})
 
 
-homepage = """
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Home page</title>
-	</head>
-	<body>
-    <p>New environment who dis</p>
-	</body>
-</html>
 """
-# 3
-
-
+A route for testing that the API is running
+"""
 @app.route('/', methods=['GET'])
 def index():
-    from flask import Flask, jsonify, request
-    return homepage
+    return 'Homepage'
 
-# get date by type
+# get post by type
 
 
-@app.route('/getdates/<dateType>/', methods=['GET'])
-def get_dates_by_type(dateType):
-    if (dateType != 'oneonone' and dateType != 'activity' and dateType != 'fooddrink'):
-        print('error. invalid date type. quitting....')
+@app.route('/getposts/<postType>/', methods=['GET'])
+def get_posts_by_type(postType):
+    if (postType != 'oneonone' and postType != 'activity' and postType != 'fooddrink'):
+        print('error. invalid post type. quitting....')
         return '', 400
-    datesByType = Dates.query.filter(Dates.type == dateType).all()
-    results = datesSchema.jsonify(datesByType)
+    postsByType = Post.query.filter(Post.type == postType).all()
+    results = postsSchema.jsonify(postsByType)
     return results
 
 
-@app.route("/uploaddate/", methods=['POST'])
-def upload_date():
+@app.route("/uploadpost/", methods=['POST'])
+def upload_post():
     jsonObject = request.get_json()
 
-    date = Dates('noid', jsonObject['title'], jsonObject['type'],
+    post = Post('noid', jsonObject['title'], jsonObject['type'],
                  jsonObject['site'], jsonObject['details'],
-                 jsonObject['reservations'], jsonObject['notes'], jsonObject['directions'])
+                 jsonObject['preparation'], jsonObject['notes'], jsonObject['location'])
 
-    db.session.add(date)
+    db.session.add(post)
     db.session.commit()
-    return "date added"
+    return "post added"
 
 
-@app.route('/updatedate/', methods=['PUT'])
-def update_date():
-    print('received update date requets')
+@app.route('/updatepost/', methods=['PUT'])
+def update_post():
+    print('received update post requets')
     jsonObject = request.get_json()
     print(jsonObject)
-    date = Dates.query.get(jsonObject['id'])
+    post = Post.query.get(jsonObject['id'])
     for key in jsonObject:
         # TODO: make request not part of state in object, rather part of state in functional components
         # id can't be changed, and request is used as state (probably change this??? in)
         if (not (key == 'id')):
-            setattr(date, key, jsonObject[key])
+            setattr(post, key, jsonObject[key])
     db.session.commit()
     return '', 204
 
 
-@app.route('/deletedate/', methods=['DELETE'])  # what method?
-def delete_date():
+@app.route('/deletepost/', methods=['DELETE'])  # what method?
+def delete_post():
     jsonObject = request.get_json()
     id = int(jsonObject)
-    dateToDelete = Dates.query.get(id)
-    if (dateToDelete == None):
-        return "Date not found", 404
+    postToDelete = Post.query.get(id)
+    if (postToDelete == None):
+        return "Post not found", 404
 
-    db.session.delete(dateToDelete)
+    db.session.delete(postToDelete)
     db.session.commit()
     return "", 204
 
@@ -159,23 +175,21 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
 # get all for debuggin
 
-@app.route('/debug/putdate/', methods=['GET'])
-def put_date():
-    date = Dates('test title', 'oneonone', 'no website',
+@app.route('/debug/putpost/', methods=['GET'])
+def put_post():
+    post = Post('test title', 'oneonone', 'no website',
                  'no details sorry debuggin', 'no res', 'no notes big  boy', 'go over there')
-    db.session.add(date)
+    db.session.add(post)
     db.session.commit()
-    return 'placed date, find it in /debug/getdates/'
+    return 'placed post, find it in /debug/getposts/'
 
 
-
-@app.route('/debug/getdates/', methods=['GET'])
-def get_dates():
-    all_dates = Dates.query.all()
-    results = datesSchema.jsonify(all_dates)
+@app.route('/debug/getposts/', methods=['GET'])
+def get_posts():
+    all_posts = Post.query.all()
+    results = postsSchema.jsonify(all_posts)
     return results
 
 
