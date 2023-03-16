23cc2188f23bde71b304eaa4e8891aa5a6dab089
Dylan Feehan <dylanfeehan981@gmail.com>
2023-03-16 15:12:02 -0400

    Add PostSpecs class for keepign consistency in the post_info fields etc

---
+++ src/assets/PostSpecs.js
@@ -0,0 +1,36 @@
+
+/**
+ * The specification for the PostSpecs in this application.
+ * Using a class like this makes it much easier to keep track of fields, as opposed to just having raw JS objects all over the place
+ */
+export default class PostSpecs {
+
+    constructor(title, type, details, preparation, notes, site, location) {
+        this.title = title;
+        this.type = type;
+        this.details = details;
+        this.preparation = preparation;
+        this.notes = notes;
+        this.site = site;
+        this.location = location;
+    }
+
+    /**
+     * Get a JSON object from the entries in this class instance (to send to API)
+     * @returns json object of entries in this class instance
+     */
+    getJSON() {
+        if(!this.enforcePostFields()) {
+            return null;
+        }
+        return Object.fromEntries(Object.entries(this));
+    }
+
+    /**
+     * Enforce that a class instance has no null fields
+     * @returns valid ? true : false
+     */
+    enforcePostFields() {
+        return Object.values(this).every((entry) => entry != null);
+    }
+}
