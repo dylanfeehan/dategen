f48eff4770db18a3de386d4f493ecbabf7682452
Dylan Feehan <dylanfeehan981@gmail.com>
2023-03-22 22:23:09 -0400

    removed old files

---
+++ src/assets/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCmJ1LUzsKq4yPPqUqQc0POTySgoNCmOeA",
+    authDomain: "dategen.firebaseapp.com",
+    projectId: "dategen", 
+    storageBucket: "dategen.appspot.com", 
+    messagingSenderId: "287951236250",
+    appId: "1:287951236250:web:7b0e06e1064e4391518323", 
+    measurementId: "G-VCHMCJQZZ7"
+};
